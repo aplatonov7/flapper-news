f19f14bae80b2401587264769e1fcc4c5452ce1b
Artem Platonov <discodesu@gmail.com>
2015-07-24 17:20:38 +0300

    refactoring

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,114 +1,104 @@
-app.controller('PostsListCtrl', [
-  '$scope',
-  'posts',
-  'auth',
-  'login',
-  function ($scope, posts, auth, login) {
-    $scope.userPost = {};
-    $scope.posts = posts;
-    $scope.auth = auth;
-    $scope.login = login;
-    $scope.currentUser = auth.currentUser();
+PostsListCtrl = function ($scope, posts, auth, login) {
+  $scope.userPost = {};
+  $scope.posts = posts;
+  $scope.auth = auth;
+  $scope.login = login;
+  $scope.currentUser = auth.currentUser();
 
-    $scope.pageSize = 3;
-    $scope.currentPage = 0;
+  $scope.pageSize = 3;
+  $scope.currentPage = 0;
 
-    $scope.$watch(function () {
-      return Math.ceil($scope.posts.container.length / $scope.pageSize)
-    }, function () {
-      $scope.totalPages = Math.ceil($scope.posts.container.length / $scope.pageSize);
-    });
+  $scope.$watch(function () {
+    return Math.ceil($scope.posts.container.length / $scope.pageSize)
+  }, function () {
+    $scope.totalPages = Math.ceil($scope.posts.container.length / $scope.pageSize);
+  });
 
-    $scope.prevPage = function () {
-      $scope.currentPage--;
-    };
+  $scope.prevPage = function () {
+    $scope.currentPage--;
+  };
 
-    $scope.nextPage = function () {
-      $scope.currentPage++;
-    };
+  $scope.nextPage = function () {
+    $scope.currentPage++;
+  };
 
-    $scope.paginationStatusNext = function () {
-      return $scope.currentPage > (Math.ceil($scope.posts.container.length / $scope.pageSize)) - 2;
-    };
+  $scope.paginationStatusNext = function () {
+    return $scope.currentPage > (Math.ceil($scope.posts.container.length / $scope.pageSize)) - 2;
+  };
 
-    angular.element(document).ready(function () {
-      componentHandler.upgradeAllRegistered();
-    });
+  angular.element(document).ready(function () {
+    componentHandler.upgradeAllRegistered();
+  });
 
-    $scope.submitForm = function () {
-      if ($scope.userPost.title) {
-        var post = {
-          title: $scope.userPost.title,
-          content: $scope.userPost.content,
-          image: $scope.userPost.image,
-          rating: 0,
-        };
+  $scope.submitForm = function () {
+    if ($scope.userPost.title) {
+      var post = {
+        title: $scope.userPost.title,
+        content: $scope.userPost.content,
+        image: $scope.userPost.image,
+        rating: 0,
+      };
 
-        posts.addPost(post);
+      posts.addPost(post);
 
-        $scope.userPost = {};
-      }
+      $scope.userPost = {};
     }
-}]);
+  }
+};
 
-app.controller('PostsCtrl', [
-  '$scope',
-  'posts',
-  'post',
-  'auth',
-  'login',
-  function ($scope, posts, post, auth, login) {
-    angular.element(document).ready(function () {
-      componentHandler.upgradeAllRegistered();
+PostsCtrl = function ($scope, posts, post, auth, login) {
+  angular.element(document).ready(function () {
+    componentHandler.upgradeAllRegistered();
+  });
+
+  $scope.post = post;
+  $scope.posts = posts;
+  $scope.auth = auth;
+  $scope.login = login;
+
+  $scope.submitForm = function () {
+    posts.addComment(
+      $scope.post,
+      $scope.userComment
+    );
+  };
+};
+
+NavCtrl = function ($scope, auth, login) {
+  $scope.login = login;
+  $scope.auth = auth;
+};
+
+AuthCtrl = function ($scope, $state, auth, login) {
+  $scope.user = {};
+
+  $scope.register = function () {
+    auth.register($scope.user).error(function (error) {
+      $scope.error = error;
+    }).then(function () {
+      login.close();
+      $scope.user = {};
     });
+  };
 
-    $scope.post = post;
-    $scope.posts = posts;
-    $scope.auth = auth;
-    $scope.login = login;
+  $scope.logIn = function () {
+    auth.logIn($scope.user).error(function (error) {
+      $scope.error = error;
+    }).then(function () {
+      login.close();
+      $scope.user = {};
+    });
+  };
 
-    $scope.submitForm = function () {
-      posts.addComment(
-        $scope.post,
-        $scope.userComment
-      );
-    };
-}]);
+  $scope.login = login;
+};
 
-app.controller('NavCtrl', [
-  '$scope',
-  'auth',
-  'login',
-  function ($scope, auth, login) {
-    $scope.login = login;
-    $scope.auth = auth;
-}]);
+PostsListCtrl.$inject = ['$scope', 'posts', 'auth', 'login'];
+PostsCtrl.$inject = ['$scope', 'posts', 'post', 'auth', 'login'];
+NavCtrl.$inject = ['$scope', 'auth', 'login'];
+AuthCtrl.$inject = ['$scope', '$state', 'auth', 'login'];
 
-app.controller('AuthCtrl', [
-  '$scope',
-  '$state',
-  'auth',
-  'login',
-  function ($scope, $state, auth, login) {
-    $scope.user = {};
-
-    $scope.register = function () {
-      auth.register($scope.user).error(function (error) {
-        $scope.error = error;
-      }).then(function () {
-        login.close();
-        $scope.user = {};
-      });
-    };
-
-    $scope.logIn = function () {
-      auth.logIn($scope.user).error(function (error) {
-        $scope.error = error;
-      }).then(function () {
-        login.close();
-        $scope.user = {};
-      });
-    };
-
-    $scope.login = login;
-  }]);
+app.controller('PostsListCtrl', PostsListCtrl);
+app.controller('PostsCtrl', PostsCtrl);
+app.controller('NavCtrl', NavCtrl);
+app.controller('AuthCtrl', AuthCtrl);
